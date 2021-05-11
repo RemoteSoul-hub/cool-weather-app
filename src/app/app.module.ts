7dacffaf73ad8c734253a30f22579e931f54f85b
RemoteSoul-hub <74861627+RemoteSoul-hub@users.noreply.github.com>
2021-05-11 17:55:36 +0000

    reverted to older commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { HomeComponent } from './components/home/home.component';
 import { CardsComponent } from './components/cards/cards.component';
-import {HttpClientModule} from '@angular/common/http';
-import {environment} from '../environments/environment';
-import {FormsModule} from '@angular/forms';
-import {NguiAutoCompleteModule} from '@ngui/auto-complete';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { DetailsComponent } from './components/details/details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    CardsComponent,
-    DetailsComponent
-ome)
- BrowserModule,
-    AppRoutingModule,
-    NguiAutoCompleteModule,
-    FormsModule,
-    ServiceWorkerModule.register('ngsw-worker.js', {enabled: environment.production})
+    CardsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
