d2fae6bfd38fa81a9127910ed1ac93b66f7d4674
RemoteSoul-hub <74861627+RemoteSoul-hub@users.noreply.github.com>
2021-05-11 17:36:46 +0000

    Details components + tons of bugs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { HomeComponent } from './components/home/home.component';
 import { CardsComponent } from './components/cards/cards.component';
+import {HttpClientModule} from '@angular/common/http';
+import {environment} from '../environments/environment';
+import {FormsModule} from '@angular/forms';
+import {NguiAutoCompleteModule} from '@ngui/auto-complete';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { DetailsComponent } from './components/details/details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    CardsComponent
+    CardsComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NguiAutoCompleteModule,
+    FormsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {enabled: environment.production})
   ],
   providers: [],
   bootstrap: [AppComponent]
