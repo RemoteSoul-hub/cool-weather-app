5c1289f356d22d3f9f5131ff1d9ab342a817d97d
RemoteSoul-hub <74861627+RemoteSoul-hub@users.noreply.github.com>
2021-05-22 15:30:40 +0100

    BUILD project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,10 +23,4 @@ export class AppComponent implements OnInit{
     });
 }
 
-modeToggleSwitch() {
- let darkActive: boolean = true;
- if (darkActive = true) {
-  console.log('dark mode on');
-}
-}
 }
